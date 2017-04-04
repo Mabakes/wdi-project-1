be64ae776e13466c73499eb4ac9db41247d5dd88
Mabakes <martha.rose.sb@gmail.com>
2017-04-04 18:11:01 +0100

    Working game - Tuesday

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var Game = Game || {};
 
 // load constants
 Game.init = function () {
-  console.log('init');
   Game.$integers       = [];
   Game.$answer         = $('.answer');
   Game.$startButton    = $('.start-game');
@@ -16,24 +15,34 @@ Game.init = function () {
   Game.count           = 0;
   Game.$eq             = $('.eq');
 
-  Game.$startButton.on('click', Game.generateNumbers);
+  // Start off by resetting the game
+  Game.reset();
+
+  // Setup eventListeners
+  Game.$eq.on('click', Game.playerSolution);
+  Game.$list.on('click', Game.playerEquation);
+};
+
+Game.reset = function() {
+  // Values
+  Game.player1 = '';
+  Game.player2 = '';
+  Game.completeSolution = '';
+  // Game.answer = '';
+  Game.operator = '';
+  // DOM selection
+  Game.$attempt1.text('');
+  Game.$attempt2.text('');
+  Game.$attemptop.text('');
+  Game.$playerSolution.text('');
+  Game.$answer.text('');
+
+  // Run again...
+  Game.generateNumbers();
 };
 
 // Generate numbers for blocks
 Game.generateNumbers = function() {
-  Game.player1 = '';
-  Game.player2 = '';
-  Game.$attempt1.text('');
-  Game.$attempt2.text('');
-  Game.$attemptop.text('');
-  // Game.$answer.text('');
-  console.log('middle' + Game.player1);
-  Game.completeSolution = '';
-  Game.answer = '';
-  Game.operator = '';
-  Game.$playerSolution.text('');
-  Game.$list.off('click', Game.playerEquation);
-  console.log('generateNumbers', Game.$list.length);
   for (let i = 0; i < Game.$list.length; i++){
     if (i < Game.$list.length/2){
       $('#id-' + (i+1)).text(Math.ceil(Math.random() * 10));
@@ -46,17 +55,16 @@ Game.generateNumbers = function() {
 };
 
 Game.generateTargetNumber = function() {
-  console.log('target');
   // Computer choses random number between 1 and 8
   Game.num1 = Math.ceil(Math.random() * 7);
   Game.num2 = Math.ceil(Math.random() * 7);
   // Gets the text from that html id:
   Game.chosenNum1 = $('#id-'+ Game.num1).text();
   Game.chosenNum2 = $('#id-'+ Game.num2).text();
-
   // Computer choses random number between 0 and 2
   // Game.chosenOp;
   Game.op = Math.ceil(Math.random() * 2);
+
   // Assigns it to chose an operation
   if (Game.op === 1){
     Game.chosenOp = '+';
@@ -66,18 +74,15 @@ Game.generateTargetNumber = function() {
 
   // Computer generates number
   Game.answer = eval(`${Game.chosenNum1} ${Game.chosenOp} ${Game.chosenNum2}`);
+
   // Assigns it to the HTML
   Game.$answer.html(Game.answer);
-
-  Game.$list.on('click', Game.playerEquation);
 };
 
 Game.playerEquation = function(){
-  console.log('playerEquation');
-
-  if($(this).hasClass('number')) {
-    if(Game.player1){
-      Game.player2=$(this).text();
+  if ($(this).hasClass('number')) {
+    if (Game.player1){
+      Game.player2 = $(this).text();
       Game.$attempt2.html(' '+Game.player2+' =');
     } else{
       Game.player1=$(this).text();
@@ -87,8 +92,6 @@ Game.playerEquation = function(){
     Game.operator = $(this).text();
     Game.$attemptop.html(' '+Game.operator+' ');
   }
-
-  Game.$eq.on('click', Game.playerSolution);
 };
 
 Game.playerSolution = function(){
@@ -100,24 +103,13 @@ Game.playerSolution = function(){
 };
 
 Game.match = function(){
-  console.log('match');
-  if(Game.completeSolution === Game.answer){
+  if (Game.completeSolution === Game.answer){
     // clear Game.player1 and Game.player2
-    console.log('before' + Game.player1);
-    // alert('Well done!');
-    Game.generateNumbers();
-  } else{
-    Game.player1 = '';
-    Game.player2 = '';
-    Game.$attempt1.text('');
-    Game.$attempt2.text('');
-    Game.$attemptop.empty();
-    // Game.$answer.text('');
-    Game.completeSolution = '';
-    Game.answer = '';
-    Game.operator = '';
-    Game.$playerSolution.text('');
+    alert('Well done!');
+  } else {
+    alert('Naaaah!');
   }
+  Game.reset();
 };
 
 $(Game.init.bind(Game));
